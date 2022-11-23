ae4bf629ec16c8da5b0e205172c5641caa8d7e15
mariafantuci <mariafantuci@gmail.com>
2022-11-23 18:58:15 -0300

    add add less btn logic

--- src/components/Buy.jsx
+++ src/components/Buy.jsx
@@ -1,14 +1,30 @@
+import { useState } from 'react'
 import { Minus, Plus, ShoppingCartSimple } from 'phosphor-react';
 
 export function Buy(){
+    const [qty, setQty] = useState(1);
+
+    function handleMinusQuantity(){
+        if(qty >= 2){
+            setQty((state) => {
+                return state - 1
+            })
+        }
+    }
+    function handlePlusQuantity(){
+        setQty((state) => {
+            return state + 1
+        })
+    }
+
     return(
         <div className="flex items-center justify-center">
             <div className="mr-2 flex items-center bg-base-button rounded-lg p-2">
-                <button className="">
-                    <Minus className="text-purple hover:text-purple-dark" size={16} />
+                <button onClick={handleMinusQuantity}>
+                    <Minus  className="text-purple hover:text-purple-dark" size={16} />
                 </button>
-                <input min="1" value="1" max="9" className="w-9 p-0 text-center bg-base-button text-base-title text-sm" type="number" />
-                <button className="">
+                <input min="1" value={qty} max="9" className="w-9 p-0 text-center bg-base-button text-base-title text-sm" type="number" />
+                <button onClick={handlePlusQuantity}>
                     <Plus className="text-purple hover:text-purple-dark" size={16} />
                 </button>
             </div>
