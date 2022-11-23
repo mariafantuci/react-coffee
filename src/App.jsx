153d953f521bea228e5253ff88788d4c3ffad505
mariafantuci <mariafantuci@gmail.com>
2022-11-23 18:04:36 -0300

    adding heading

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ export function App() {
     <div className="">
       <Header />
       <Hero />
+      <h2 className="px-10 lg:px-36 font-baloo font-extrabold text-2xl leading-10 mb-14">Nossos cafés</h2>
       <div className="px-10 lg:px-36 w-full grid lg:grid-cols-2 xl:grid-cols-4 gap-y-8 gap-x-7">
       { 
         coffees.map(coffee => {
