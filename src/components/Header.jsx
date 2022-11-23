fa4709ecae51e330227ddafe5639da300f3974de
mariafantuci <mariafantuci@gmail.com>
2022-11-23 17:39:10 -0300

    adding more padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { MapPin, ShoppingCart } from 'phosphor-react'
 
 export function Header () {
     return (
-        <header className="flex w-full bg-background py-10 px-10 justify-between items-center">
+        <header className="flex w-full bg-background py-10 px-10 lg:px-36 justify-between items-center">
             <img src={logo} alt="coffee purple" />
             <div className="flex items-center gap-3">
                 <div className="flex items-center bg-purple-light text-purple rounded-lg p-2 gap-1">
