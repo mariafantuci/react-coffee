8045a3a1038da4e772140e96975463d01aa78ee2
mariafantuci <mariafantuci@gmail.com>
2022-11-24 12:34:40 -0300

    Adding home page file

---
+++ src/pages/Home/Index.jsx
@@ -0,0 +1,23 @@
+import { Card } from '../../components/Card'
+import { Header } from '../../components/Header'
+import { Hero } from '../../components/Hero'
+import { coffees } from '../../database/coffees'
+
+export function Home(){
+    return (
+        <div className="">
+            <Header />
+            <Hero />
+            <h2 className="px-10 lg:px-36 font-baloo font-extrabold text-2xl leading-10 mb-14">Nossos cafés</h2>
+            <div className="px-10 lg:px-36 w-full grid lg:grid-cols-2 xl:grid-cols-4 gap-y-8 gap-x-7">
+                { 
+                    coffees.map(coffee => {
+                    return( coffee.stock &&
+                        <Card key={coffee.id} coffee={coffee} />
+                    )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
