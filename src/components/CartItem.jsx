66da6ef962b7d9a48667f40e6d6c38738aa5eb2d
mariafantuci <mariafantuci@gmail.com>
2022-12-07 11:22:45 -0300

    form component

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,7 @@
+export function CartItem(){
+    return (
+        <div className="">
+            
+        </div>
+    )
+}
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,19 @@
+import { CartItem } from './CartItem';
+
 export function Form(){
     return (
         <div className="py-10 px-10 lg:px-36">
-        <h2 className="">Complete seu pedido</h2>
-            <form>
-
-            </form>
+            <div className="">
+                <h2 className="font-baloo font-bold text-md leading-6 mb-6 ">Complete seu pedido</h2>
+                <form>
+                </form>
+            </div>
+            <div className="">
+                <h2 className="font-baloo font-bold text-md leading-6 mb-6 ">Cafés selecionados</h2>
+                <form>
+                    <CartItem />
+                </form>
+            </div>
         </div>
     )
 }
