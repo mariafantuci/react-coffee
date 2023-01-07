c8b624ace470510dd8a7af6f47fc8cf58407ce8f
mahfantuci <mariafantuci@gmail.com>
2023-01-07 19:57:07 -0300

    Adding and configured a new component

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,9 +1,21 @@
+import { coffees } from '../../database/coffees';
+import { Card } from './components/Card';
 import { Hero } from './components/Hero';
 
 export function Home(){
     return(
         <div className="">
             <Hero />
+            <h2 className="px-10 lg:px-36 font-baloo font-extrabold text-2xl leading-10 mb-14">Nossos cafés</h2>
+            <div className="px-10 lg:px-36 w-full grid lg:grid-cols-2 xl:grid-cols-4 gap-y-8 gap-x-7">
+            { 
+                coffees.map(coffee=> {
+                return(
+                        <Card key={coffee.id} coffee={coffee} />
+                    )
+                })
+            }
+            </div>
         </div>
     )
 }
---
+++ src/pages/Home/components/Card.tsx
@@ -0,0 +1,31 @@
+import { Buy } from '../../../components/Buy'
+import { Image } from '../../../components/Image'
+import { coffeesType } from '../../../database/coffees'
+
+interface coffeeProps {
+    coffee: coffeesType
+}
+
+export function Card({ coffee }: coffeeProps) {
+    return (
+        <div className="box-border bg-base-card rounded-tl-md rounded-bl-large rounded-br-md rounded-tr-large flex justify-center flex-col w-full items-center pb-5 px-4 lg:px-5 xl:px-8 mb-10 lg:mb-12">
+            <Image className="w-32 h-32 -mt-9" src={coffee.img} alt="black coffee" />
+            <div className="flex w-full justify-center gap-1">
+                {
+                    coffee.tags.map(tag => {
+                        return(
+                            <span key={tag} className="text-yellow-dark uppercase font-bold text-2xxs bg-yellow-light py-1 px-2 rounded-full">{tag}</span>
+                        )
+                    })
+                }
+            </div>
+            <h3 className="font-baloo font-bold text-lg mt-4 mb-2">{coffee.name}</h3>
+            <p className="mb-8 text-center text-base-label text-xs leading-4">{coffee.description}</p>
+            <div className="flex w-full items-center align-center content-center justify-center">
+                <span className="text-base-text text-xs mr-1">R$:</span>
+                <span className="font-baloo font-bold text-xs text-base-text mr-5">{coffee.price}</span>
+                <Buy />
+            </div>
+        </div>
+    )
+}
